fe22569ba1ffa7723c623830e1850475c4e3948d
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-05-29 00:34:58 +0300

    Add TaskService.CreateTask() empty title validation

--- TaskManager/Services/TaskService.cs
+++ TaskManager/Services/TaskService.cs
@@ -32,9 +32,15 @@ namespace TaskManager.Services
         public async Task<TaskItem> CreateTask(TaskItem item)
         {
             var existingTask = await _repository.GetTaskByTitle(item.Title);
+
+            if(string.IsNullOrWhiteSpace(item.Title)) {
+                throw new BusinessException("TASK_TITLE_INVALID", "Title cannot be empty");
+            }
+
             if (existingTask != null) {
                 throw new BusinessException("TASK_ALREADY_EXISTS", "Task already exists");
             }
+            
             return await _repository.CreateTask(item);
         }
 
